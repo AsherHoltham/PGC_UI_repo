2d74d5b504855c8ddde89aa17a824f7508daaaa1
AsherHoltham <asher.holtham.github@gmail.com>
2025-02-24 13:47:20 -0800

    BACKEND: TRIP MODEL SETUP PRE DATABASE INIT

--- pgc_scaffold/lib/data/models/models.dart
+++ pgc_scaffold/lib/data/models/models.dart
@@ -1,1 +1,49 @@
-//*** SQFLITE DATABASE SCHEMAS ***//
+//*** Trips SQFLITE DATABASE SCHEMAS ***//
+const String tableName = 'trips';
+
+const String idField = '_id';
+const String titleField = 'title';
+const String descriptionField = 'description';
+
+const List<String> tripColumns = [idField, titleField, descriptionField];
+
+const String boolType = "BOOLEAN NOT NULL";
+const String idType = "INTEGER PRIMARY KEY AUTOINCREMENT";
+const String textTypeNullable = "TEXT";
+const String textType = "TEXT NOT NULL";
+
+class TripModel {
+  int? id;
+  String title;
+  String description;
+
+  TripModel({
+    this.id,
+    required this.title,
+    required this.description,
+  });
+  // Convert a YourModel instance into a Map.
+  Map<String, dynamic> toJson() => {
+        '_id': id,
+        'title': title,
+        'description': description,
+      };
+
+  // Create a YourModel instance from a Map.
+  static TripModel fromJson(Map<String, dynamic> json) => TripModel(
+        id: json['_id'] as int?,
+        title: json['title'] as String,
+        description: json['description'] as String,
+      );
+
+  TripModel copyWith({
+    int? id,
+    String? title,
+    String? description,
+  }) =>
+      TripModel(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        description: description ?? this.description,
+      );
+}
