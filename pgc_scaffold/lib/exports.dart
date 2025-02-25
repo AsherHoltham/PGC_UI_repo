36bb683a130f97513cddeafd816e98a18f5a89ed
AsherHoltham <asher.holtham.github@gmail.com>
2025-02-24 18:35:23 -0800

    UTILS: REMOVED UTILS

--- pgc_scaffold/lib/exports.dart
+++ pgc_scaffold/lib/exports.dart
@@ -5,7 +5,6 @@ export 'package:flutter_svg/flutter_svg.dart';
 export 'package:google_fonts/google_fonts.dart';
 export 'package:animations/animations.dart';
 
-export 'utils/screen_size.dart';
 export 'pages/personal_group_coordinator.dart';
 export 'pages/trip_page.dart';
 export 'widgets/trip_info_card.dart';
--- pgc_scaffold/lib/main.dart
+++ pgc_scaffold/lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    ScreenSize.init(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'PGC',
